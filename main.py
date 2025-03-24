b88799796294d69c17c87b0a06b0620d2238948e
Seyeon Joo <joosy1964@gmail.com>
2025-03-24 10:39:49 +0900

    설정 수정

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello():
+    return {"message": "Hello 세연"}
